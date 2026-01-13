d4a1d8017670d39eae64725087602ff2b413f632
joao.chagas <joao.chagas@supermercadosmundial.com>
2026-01-13 16:25:49 -0300

    adicao de core de autenticacao e reservas

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import { provideRouter, withComponentInputBinding } from '@angular/router';
-import { routes } from './app/app.routes';
-import { provideAnimations } from '@angular/platform-browser/animations';
-import { provideHttpClient } from '@angular/common/http';
+import { appConfig } from './app/app.config';
 
-bootstrapApplication(AppComponent, {
-  providers: [
-    provideRouter(
-      routes, 
-      withComponentInputBinding()
-    ),
-    provideAnimations(),
-    provideHttpClient(), 
-  ]
-}).catch(err => console.error(err));
+bootstrapApplication(AppComponent, appConfig)
+  .catch(err => console.error(err));
